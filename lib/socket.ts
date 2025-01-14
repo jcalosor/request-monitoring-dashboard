f6c0d5d93a9d9c1bf715e821e2260328365647f3
Jerric Calosor <j.calosor@gmail.com>
2025-01-14 22:09:29 +0800

    feature/refactor-real-time-feature
    - Refactor pusher implementation, fix functionality

--- lib/socket.ts
+++
@@ -1,5 +0,0 @@
-import { io } from "socket.io-client";
-import config from "./config";
-
-const socket = io(`http://localhost:${config.api_port}`); // Replace with your backend URL
-export default socket;
